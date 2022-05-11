18988ea3f34fc0e0b05fc3e13770832ae3db3c69
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-05-11 20:12:47 +0500

    model theme add

---
+++ lib/constant/theme_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Color kbgBlue = Color(0xFF3B66BE);
+const Color bgButtonWhite = Color(0xffffffff);
+
+const Color white = Color(0xffffffff);
+const Color lightBlue = Color(0xff41b9eb);
+const kTextColor = Color(0xff757575);
+const kPrimaryColor = Color(0xffff7643);
+
+
+final privacyHeadingStyle = TextStyle(fontSize: 15,fontWeight: FontWeight.w400,fontFamily: 'Poppins',color: Colors.black);
+final registerHeadingStyle = TextStyle(fontSize: 20,fontWeight: FontWeight.w600,fontFamily: 'Poppins',color: Colors.black);
+final memberHeadingStyle = TextStyle(fontSize: 19,fontWeight: FontWeight.w400,fontFamily: 'Poppins',color: Colors.black);
+final forgetPassword = TextStyle(fontSize: 13,fontWeight: FontWeight.w400,fontFamily: 'Poppins',color: Colors.black,);
+
